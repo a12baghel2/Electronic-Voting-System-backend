b9567830264f129ff91ad4adf15d0881729bbd2f
Abhimanyu <43077595+a12baghel2@users.noreply.github.com>
2022-05-14 09:48:51 +0530

    minor changes

--- src/main/java/com/ElectronicVotingSystem/controller/AuthenticationController.java
+++ src/main/java/com/ElectronicVotingSystem/controller/AuthenticationController.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,7 @@ import com.ElectronicVotingSystem.model.AuthenticationRequest;
 import com.ElectronicVotingSystem.model.AuthenticationResponse;
 import com.ElectronicVotingSystem.model.UserDTO;
 
+@CrossOrigin("*")
 @RestController
 public class AuthenticationController {
 
